397ec0f1d27fea824ea4325cd33568fa38b86c7d
AliHaidry <alihaidry11@gmail.com>
2021-08-07 14:10:20 -0400

    Code Updated

--- app.py
+++ app.py
@@ -26,5 +26,30 @@ def list1():
     return render_template("base.html", todo_list=todo_list)
 
 
+@app.route('/add', methods=["POST"])
+def add():
+    title = request.form.get("title")
+    new_todo = Todo(title=title, complete=False)
+    db.session.add(new_todo)
+    db.session.commit()
+    return redirect(url_for("home1"))
+
+
+@app.route('/update/<int:todo_id>')
+def update(todo_id):
+    todo = Todo.query.filter_by(id=todo_id.first())
+    todo.complete = not todo.complete
+    db.session.commit()
+    return redirect(url_for("home1"))
+
+
+@app.route('/delete/<int:todo_id>')
+def delete(todo_id):
+    todo = Todo.query.filter_by(id=todo_id.first())
+    db.session.delete(todo)
+    db.session.commit()
+    return redirect(url_for("home1"))
+
+
 if __name__ == "__main__":
     app.run()
